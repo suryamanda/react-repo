2b8211376c1c84d09b70a558c15874704ad2fb30
SURYA <suryamss09@gmail.com>
2020-06-25 10:10:33 +0530

    state property usage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component{
+
+  state = {
+    persons : [
+      { name : 'Max', age : 28},
+      { name : 'Manu', age : 29},
+      { name : 'Stefan', age : 26},
+    ]
+  }
+
 render(){
  return (
       <div className="App">
       <h1>Hi, I'm a react app</h1>
-      <Person person="Max" age = "28"/>
-      <Person person="Manu" age = "30"/>
-      <Person person="Stefan" age = "27"> My Hobbies : Racing</Person>
+      <Person name={this.state.persons[0].name} age = {this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age = {this.state.persons[1].age}/>
+      <Person name={this.state.persons[2].name} age = {this.state.persons[2].age}/>
+      <Person name="Stefan" age = "27"> My Hobbies : Racing</Person>
     </div>
  );
 }
