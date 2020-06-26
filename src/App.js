6f15dcf29c0c4b15b149c88d388a9dcd7e636c31
SURYA <suryamss09@gmail.com>
2020-06-26 12:54:26 +0530

    inline styles

--- src/App.js
+++ src/App.js
@@ -34,10 +34,21 @@ class App extends Component {
 
 //we can pass method references as params
 render(){
+
+  const style = {
+    backgroundColor : 'white',
+    font : 'inherit',
+    border: '1px solid blue',
+    padding : '8px',
+    cursor : 'pointer'
+  };
+
  return (
       <div className="App">
       <h1>Hi, I'm a react app</h1>
-      <button onClick = { () => this.switchNameHandler('Surya')}>Switch Name</button>
+      <button 
+      style={style}
+      onClick = { () => this.switchNameHandler('Surya')}>Switch Name</button>
       <Person
        name={this.state.persons[0].name}
        age = {this.state.persons[0].age}/>
