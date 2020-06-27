29b2efa463821904742454b4bae0d7718f4cde8c
SURYA <suryamss09@gmail.com>
2020-06-27 11:39:17 +0530

    Flexible lists, updating lists and getting elements based on condition

--- src/App.js
+++ src/App.js
@@ -14,12 +14,24 @@ class App extends Component {
     showPersons : false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({persons : [
-      { name : 'Max', age : 28},
-      { name : 'Manu', age : 29},
-      { name : event.target.value, age : 26}
-    ] });
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id == id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+  //const person = Object.assign({}, this.state.persons[personIndex]);
+
+  person.name = event.target.value;
+
+  const persons = [...this.state.persons];
+  persons[personIndex] = person;
+
+
+    this.setState({persons : persons});
   }
 
   deletePersonHandler = (index) => {
@@ -62,6 +74,7 @@ render(){
       name={person.name}
       age = {person.age}
       key = {person.id}
+      changed = {(event) => this.nameChangedHandler(event, person.id)}
       />
     }
       )};
